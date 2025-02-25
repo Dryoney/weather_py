438483de5151d3f97951a4d17fb5949421192f5c
dryoney <bitytski.aleks@gmail.com>
2025-02-25 21:25:55 +0500

    Исправление замечаний(by AlexanderZharyuk)

--- main.py
+++ main.py
@@ -1,26 +1,57 @@
+import os
 import requests
-from datetime import datetime, timedelta
 
-print("В каком городе вы хотите узнать температуру: ")
-user_input = input ()
-city=str(user_input)
+from datetime import datetime,timedelta
+from dotenv import load_dotenv
 
-appid = 'ffc82bea7fa6bc2bb4d0ec70cf02f7a5'
-today_url = f'http://api.openweathermap.org/data/2.5/weather?q={city}&appid={appid}&units=metric'
 
-tomorrow_url = f"https://api.openweathermap.org/data/2.5/forecast?q={city}&appid={appid}&units=metric"
 
-response1 = requests.get(today_url)
-data1 = response1.json()
-today_temp = data1['main']['temp']
-today_description = data1['weather'][0]['description']
-print(f'Погода на сегодня в городе {city}: {today_temp}°C, {today_description}.')
+def user_input():
+    print("В каком городе вы хотите узнать температуру: ")
+    user_input = input()
+    city=str(user_input)
+    if not city.isalpha():
+        print ('Неверное название города')
+    return city
 
-response2 = requests.get(tomorrow_url)
-data2 = response2.json()
-tomorrow_timestamp = int((datetime.now() + timedelta(days=1)).timestamp())
-for forecast in data2['list']:
-        if forecast['dt'] >= tomorrow_timestamp and forecast['dt'] < tomorrow_timestamp + 86400:
-            tomorrow_temp = forecast['main']['temp']
-            tomorrow_description = forecast['weather'][0]['description']
-print(f'Погода на завтра в городе {city}: {tomorrow_temp}°C, {tomorrow_description}.')
+def params_indef(city):
+    load_dotenv()
+    key = os.getenv("appkey")
+    url_params = {
+        'appid' : key,
+        'q' : city,
+        'units' : 'metric'
+    }
+    return url_params
+
+def now_weather(url_params):
+    url = 'https://api.openweathermap.org/data/2.5/weather'
+    try:
+        response = requests.get(url,params = url_params)
+    except requests.ConnectionError:
+        print ('<сетевая ошибка>')
+    data = response.json()
+    today_temp = data['main']['temp']
+    today_description = data['weather'][0]['description']
+    print(f'Погода на сегодня в городе {url_params['q']}: {today_temp}°C, {today_description}.')
+
+def tomorrow_weahter(url_params):
+    url = "https://api.openweathermap.org/data/2.5/forecast"
+    try:
+        responseTomorrow = requests.get(url,params = url_params)
+    except requests.ConnectionError:
+        print ('<сетевая ошибка>')
+    dataTomorrow = responseTomorrow.json()
+    tomorrow_timestamp = int((datetime.now() + timedelta(days=1)).timestamp())
+    for forecast in dataTomorrow['list']:
+            if forecast['dt'] >= tomorrow_timestamp and forecast['dt'] < tomorrow_timestamp + 86400:
+                tomorrow_temp = forecast['main']['temp']
+                tomorrow_description = forecast['weather'][0]['description']
+                break
+    print(f'Погода на завтра в городе {url_params['q']}: {tomorrow_temp}°C, {tomorrow_description}.')
+def main():
+    city = user_input()
+    params = params_indef(city)
+    now_weather(params)
+    tomorrow_weahter(params)
+main()
